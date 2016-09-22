8b4311ef3739ffacd233d2d0c0059cde62fb4d54
fabrifs <fabrizzio.fs@gmail.com>
2016-09-22 13:20:52 -0300

    Update marvel.js
    Alteração data

--- AppMobile/WebContent/appMarvel.js
+++ AppMobile/WebContent/appMarvel.js
@@ -1,35 +1,34 @@
-function rola (){
-
+function consultaMarvel (){
+    
 var apikey = "62cc7f7bd41e3346a1af737e0449428b";
-var privatekey = "a9e5cccf7acc7f9ce2555471b5b6fc51d5725df6";
-var urlbase = "http://gateway.marvel.com/v1/public/characters";
-var today= 	new Date(year, month, day, hours, minutes, seconds, milliseconds);
+		var privatekey = "a9e5cccf7acc7f9ce2555471b5b6fc51d5725df6";
+		var urlbase = "http://gateway.marvel.com/v1/public/characters";
+		var today= 	new Date();
 
 
-var dd = today.getDate();
-var mm = today.getMonth()+1; //January is 0!
-var yyyy = today.getFullYear();
-var hh = today.getHours();
-var min = today.getMinutes();
-var sec = today.getSeconds();
+		var dd = today.getDate();
+		var mm = today.getMonth()+1; //Janeiro é 0
+		var yyyy = today.getFullYear();
+		var hh = today.getHours();
+		var min = today.getMinutes();
+		var sec = today.getSeconds();
 
-if(dd<10){
-    dd='0'+dd
-} 
-if(mm<10){
-    mm='0'+mm
-} 
+		if(dd<10){
+			dd='0'+dd
+		} 
+		if(mm<10){
+			mm='0'+mm
+		} 
 
-var dataSt = mm+""+dd+""+yyyy+""+hh+""+min+""+sec;
-
-
-var player1 = document.getElementsByName("player1");
-var player2 = document.getElementsByName("player2");
+		var dataSt = mm+""+dd+""+yyyy+""+hh+""+min+""+sec;
 
 
+		var player1 = document.getElementById("player1");
+		var player2 = document.getElementById("player2");
+		
+		console.log("player1:" + player1);
 
-var uri1 = urlbase + "?nameStartsWith=" + player1 + "&ts=" + dataSt + "&apikey=" + apikey + "&hash=" + hashStr;
-
+		var uri1 = urlbase + "?nameStartsWith=" + player1 + "&ts=" + dataSt + "&apikey=" + apikey + "&hash=" + hashStr;
 
 
 }
