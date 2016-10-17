6809d3ba9b8fb87b03abdabbd846b4ff833f9020
Vrauliver Queen <thipex@hotmail.com>
2016-10-17 19:50:26 -0200

    Update appMarvel.js
    Adicionado tabela

--- AppMobile/WebContent/appMarvel.js
+++ AppMobile/WebContent/appMarvel.js
@@ -40,11 +40,80 @@ function consultaMarvel(player1, player2){
     var p2 = JSON.parse(xmlHttp.responseText);
 
     if(p1.code == 200 && p2.code ==200){
-        alert("Consulta OK");
-        document.getElementById("teste").innerHTML = p1.code;
+       // alert("Consulta OK");
+        
+        var eventsp1 = p1.data.results[0].events.available;
+        var comicsp1 = p1.data.results[0].comics.available;
+        var seriesp1 = p1.data.results[0].series.available;
+        
+        var eventsp2 = p2.data.results[0].events.available;
+        var comicsp2 = p2.data.results[0].comics.available;
+        var seriesp2 = p2.data.results[0].series.available;
+        
+        if(player1.includes("%20")){
+            player1 = player1.replace("%20", " ");
+        }
+        
+        
+        if(player2.includes("%20")){
+            player2 = player2.replace("%20", " ");
+        }
+        
+        
+        var tabela = "<table border = 1>"+
+            "<th></th>"+
+            "<th>"+player1+"</th>"+
+            "<th>"+player2+"</th>"+
+            "<tr><td>Comics</td><td>"+comicsp1+"</td><td>"+comicsp2+"</td></tr>"+
+            "<tr><td>Series</td><td>"+seriesp1+"</td><td>"+seriesp2+"</td></tr>"+
+            "<tr><td>Events</td><td>"+eventsp1+"</td><td>"+eventsp2+"</td></tr>"+
+        "</table>";
+        
+        
+        var countp1 = 0;
+        var countp2 = 0;
+        
+        if(eventsp1 > eventsp2){
+            countp1++;
+        }else if(eventsp2 > eventsp1){
+            countp2++;
+        }
+        
+        
+        if(comicsp1 > comicsp2){
+            countp1++;
+        }else if(comicsp2 > comicsp1){
+            countp2++;
+        }
+        
+        
+        if(seriesp1 > seriesp2){
+            countp1++;
+        }else if(seriesp2 > seriesp1){
+            countp2++;
+        }
+        
+        var winner = "";
+        var foto = "";
+        
+        if(countp1 > countp2){
+            foto =  p1.data.results[0].thumbnail.path+".jpg";
+            winner = "<h1>"+ player1 + " Venceu!</h1><br /><img src='"+foto + "' alt='player1' width = '25%' height='25%'/>";
+        }else if(countp1 < countp2){
+            foto =  p2.data.results[0].thumbnail.path+".jpg";
+            winner = "<h1>"+ player2 + " Venceu!</h1><br /><img src='"+foto + "' alt='player2' width = '25%' height='25%'/>";
+        }else{
+            winner = "<h1>Nesta batalha não houveram vencedores!</h1>";
+        }
+                
+        document.getElementById("resultado").innerHTML = winner + tabela;
+        
+        
+        
     
     
     }else{
-        alert("Erro ao consultar Marvel");
+        var errrow="..\\img\\errow.jpg";
+         document.getElementById("resultado").innerHTML = "<img src='"+errrow+"' alt='errrow' width='90%' height='90%'>";
     }
 };
